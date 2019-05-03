b5541a1ff3d147acc6f9aa696ea10d25025383cb
crustalmaa <crustalmaa@gmail.com>
2019-05-02 18:20:49 -0700

    header test

---
+++ src/component/header/header.spec.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import Header from './index';
+import {findByTestAttr} from '../../../Utils/index';
+
+const setUp =(props={})=>{
+    const component = shallow(<Header {...props}/>)
+    return component;
+}
+
+describe('Header component', ()=>{
+    let component;
+    beforeEach(()=>{
+        component = setUp();
+    });
+    describe('Nested',  ()=>{
+        it('it should render without errors', ()=>{
+            
+            const wrapper = findByTestAttr(component, 'headerComponent');
+            expect(wrapper.length).toBe(1);
+        })
+
+        it('it should render a logo', ()=>{
+            const wrapper = findByTestAttr(component, 'headerComponent');
+            expect(wrapper.length).toBe(1); 
+        })
+    })
+})
