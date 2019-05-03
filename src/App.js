070a3495c3d24dd7d4f215aa653f44b955b4ada6
crustalmaa <crustalmaa@gmail.com>
2019-05-02 17:25:18 -0700

    with test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Header from './component/header/index';
 function App() {
   return (
     <div className="App">
-      
+          <Header />
     </div>
   );
 }
